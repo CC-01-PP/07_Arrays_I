d196dd5be06c620e452f38977d287ae49973b113
Mia Kamenetski <mia.kamenetski@online.de>
2022-11-15 11:00:59 +0100

    02a. Theorie Schleifen (for-schleife)

--- script_01.js
+++ script_01.js
@@ -5,22 +5,42 @@
 
 /*** 02. Theorie: Array */
 
-let arr;
-//arr = new Array(); // Konstruktor
-arr = []; // Literal
-arr = [2,7,11,4];
-arr = [false, true, false];
-arr = ["Ich", "bin", "Max", "Mütze"];
+// let arr;
+// //arr = new Array(); // Konstruktor
+// arr = []; // Literal
+// arr = [2,7,11,4];
+// arr = [false, true, false];
+// arr = ["Ich", "bin", "Max", "Mütze"];
 
-output(arr);
-output(arr.length); // Anzahl der Elemente
-output(arr[0]); // Inex 0 (1. Pos. im Array)
-output(arr[3]); // Letze Pos. / konkret
-output(arr[arr.length-1]); // Leze Pos. / allgemein
+// output(arr);
+// output(arr.length); // Anzahl der Elemente
+// output(arr[0]); // Inex 0 (1. Pos. im Array)
+// output(arr[3]); // Letze Pos. / konkret
+// output(arr[arr.length-1]); // Leze Pos. / allgemein
 
 //() --> Parameter
 //{} --> Codeblöcke
 //[] --> Arrays, Index
+
+/*** 02a. Theorie Schleifen (for-schleife) */
+
+/*For-Schleifen als allg. Wiederholungs-Struktur */
+// Inkrement (untere Grenze --> obere Grenze)
+// for (let i = 0; i < 4; i++) {
+//     output("index i: " + i)    
+// }
+
+//Dekrement (obere Grenze --> untere Grenze)
+// for (let i = 10; i > 0 ; i--) {
+//     output("index i: " + i)
+// }
+
+// Inkrement (var. Schrittweite)
+// for (let i = 0; i <= 100; i+=10) {
+//     output("index i: " + i);
+// }
+
+
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
@@ -48,4 +68,22 @@ function output(outputStr) {
     console.log(outputStr);
 }
 
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1, word2, .... wordN :: Semantische Struktur
+// arr[0], arr[1], .... arr[n] :: Numerische Struktur
+// --> Transformation semantisches Problem --> numerisches Problem
+// output(getSentence(["Ich", "bin", "die", "coole", "Maxine", "Mützerich"]));
+function getSentence (arr) {
 
+    const gap   = " ";
+    const punct = ".";
+    let str     =   arr[0] + gap +
+                    arr[1] + gap +
+                    arr[2] + gap +
+                    arr[3] + gap +
+                    arr[4] + gap +
+                    arr[5] +
+                    punct;
+    return str;
+}
